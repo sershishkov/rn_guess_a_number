99eab2c21ff2d15fd3005246ddba792efe1ddfeb
sershishkov <ser.shishkov@gmail.com>
2019-09-15 09:47:21 +0300

    4-35

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -83,8 +83,8 @@ const GameScreen = props => {
           <Ionicons name='md-add' size={24} color='white' />
         </MainButton>
       </Card>
-      <View style={styles.list}>
-        <ScrollView>
+      <View style={styles.listContainer}>
+        <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) =>
             renderListItem(guess, pastGuesses.length - index)
           )}
@@ -108,9 +108,14 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     marginTop: 20
   },
-  list: {
+  listContainer: {
     flex: 1,
     width: '80%'
+  },
+  list: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end'
   },
   listItem: {
     borderColor: '#ccc',
@@ -119,7 +124,8 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     backgroundColor: 'white',
     flexDirection: 'row',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    width: '60%'
   }
 });
 
