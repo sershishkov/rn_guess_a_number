5fde77d7a511db9e2bc507212a585101242377df
sershishkov <ser.shishkov@gmail.com>
2019-09-15 14:20:25 +0300

    5-16

--- components/Header.js
+++ components/Header.js
@@ -5,22 +5,35 @@ import TitleText from '../components/TitleText';
 
 const Header = ({ title }) => {
   return (
-    <View style={styles.header}>
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid
+        })
+      }}
+    >
       <TitleText style={styles.title}>{title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
     justifyContent: 'center',
-    alignItems: 'center',
-    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
+    alignItems: 'center'
+  },
+  headerIOS: {
+    backgroundColor: 'white',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary
   },
   title: {
     color: Platform.OS === 'ios' ? Colors.primary : 'white'
